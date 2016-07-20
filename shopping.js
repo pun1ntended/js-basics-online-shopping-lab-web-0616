9aee353769ad088148567c29949115b95cca0808
Steven McGrath <mcgrath.steven7@gmail.com>
2016-07-20 19:10:49 -0400

    Done.

--- shopping.js
+++ shopping.js
@@ -1,11 +1,66 @@
 var cart;
+cart = [];
 
 function setCart(newCart) {
   cart = newCart;
 }
+function getCart() {
+  return cart;
+}
+function addToCart(item) {
+  var price;
+  price = Math.floor(Math.random() * 101);
+  var list_item;
+  list_item = {[item]: price};
+  cart.push(list_item);
+
+  console.log(`${item} has been added to your cart.`);
+
+  return cart;
+}
+
+
+function viewCart() {
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  }
+  var item_list = [];
+  for (var i = 0; i < cart.length; i++) {
+    var current_item = cart[i];
+    var item = Object.keys(current_item)[0];
+    var price = current_item[item];
+    item_list.push(` ${item} at $${price}`);
+  }
+  console.log(`In your cart, you have${item_list}.`);
+}
+function removeFromCart(item) {
+  let item_status = false;
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      item_status = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1))
+      item_status = true;
+    }
+
+  }
+  if (!item_status) {
+    console.log(`That item is not in your cart.`)
+  }
+  return cart;
+
+}
+
+function placeOrder(creditCard) {
+  if (!creditCard) {
+    return console.log('We don\'t have a credit card on file for you to place your order.');
+  }
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${creditCard}.`);
+  cart = [];
+  return cart;
+}
 
 function total() {
-  let t = 0
+  let t = 0;
 
   for (var i = 0, l = cart.length; i < l; i++) {
     for (var item in cart[i]) {
@@ -15,3 +70,4 @@ function total() {
 
   return t
 }
+
--- test/shopping-test.js
+++ test/shopping-test.js
@@ -10,7 +10,6 @@ chai.use(spies)
 
 const expect = chai.expect
 
-const errorMessage = "Should have called `console.log()` with the appropriate string."
 
 describe('shopping', () => {
   jsdom({
@@ -39,8 +38,7 @@ describe('shopping', () => {
         been.
         called.
         with(
-          "pizza has been added to your cart.",
-          "Should have called `console.log()` with 'pizza has been added to your cart.'");
+          "pizza has been added to your cart.");
 
       console.log.reset()
     })
@@ -70,8 +68,8 @@ describe('shopping', () => {
         been.
         called.
         with(
-          `In your cart, you have socks at $${socksCost}, puppy at $${puppyCost}, iPhone at $${iPhoneCost}.`,
-          errorMessage
+          `In your cart, you have socks at $${socksCost}, puppy at $${puppyCost}, iPhone at $${iPhoneCost}.`
+
         );
       console.log.reset()
     });
@@ -91,7 +89,7 @@ describe('shopping', () => {
       const puppyCost = getCart()[1]["puppy"];
       const iPhoneCost = getCart()[2]["iPhone"];
 
-      var totalCost = socksCost + puppyCost + iPhoneCost
+      var totalCost = socksCost + puppyCost + iPhoneCost;
 
       expect(total()).to.equal(totalCost)
     })
@@ -118,8 +116,7 @@ describe('shopping', () => {
         been.
         called.
         with(
-          "That item is not in your cart.",
-          errorMessage
+          "That item is not in your cart."
         );
     });
   });
@@ -136,8 +133,7 @@ describe('shopping', () => {
         been.
         called.
         with(
-          "We don't have a credit card on file for you to place your order.",
-          errorMessage
+          "We don't have a credit card on file for you to place your order."
         );
 
       console.log.reset()
@@ -158,8 +154,7 @@ describe('shopping', () => {
         been.
         called.
         with(
-          `Your total cost is $${t}, which will be charged to the card 123.`,
-          errorMessage
+          `Your total cost is $${t}, which will be charged to the card 123.`
         );
 
       console.log.reset()
